9008a0e8074916a57d868289922c4a2a7966671b
Solomon <sholasolomon@yahoo.com>
2023-06-02 18:34:12 +0100

    First commit with Jquery

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+// Selecting content with jquery
+
+// $("h3").css({border: "3px solid blue"});
+
+// //Selecting classes
+// $(".wrapper").css({border: "2px solid lightblue"})
+
+// $("#clients").css({border: "3px solid red"})
+
+//JQuery FILTERS
+
+$("header nav li:first").css({border: "2px solid red"})
+$("header nav li:last").css({border:"3px solid yellow"})
