88bf430c799229c803e844fccbb2a4b2ad2d4eb2
Solomon <sholasolomon@yahoo.com>
2023-06-03 00:25:56 +0100

    Checking jquery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,5 +9,26 @@
 
 //JQuery FILTERS
 
-$("header nav li:first").css({border: "2px solid red"})
-$("header nav li:last").css({border:"3px solid yellow"})
+// $("header nav li:first").css({border: "2px solid red"})
+// $("header nav li:last").css({border:"3px solid yellow"})
+// $("img[alt=quote").css({border: "2px solid red"})
+
+//$("#contact-methods").css({border: "2px solid red"})
+// $("#social-nav").prev().css({border: "2px solid blue"})
+//$(".banner-title").parents().css({border: "2px solid pink"})
+// $("#social-nav").children().css({border: "2px solid green"})
+// $("#contact").find(".twitter").css({border: "2px solid red"})
+
+//CHAINING
+// $("#contact-methods").css({border: "2px solid green"}).next().css({border: "2px solid brown"})
+//  .closest("section").css({border:"2px solid red"})
+
+// ADDING  AND CHANGING CONTENT OF DIVS
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'> The big fight live: Ham vs Cheese! </div>"
+
+//$("#tweets div").append(tweet)
+//$("#tweets div").prepend(tweet)
+
+//REPLACE THE CONTENT OF <p> tag
+
+$("#tweets div").html(tweet)
